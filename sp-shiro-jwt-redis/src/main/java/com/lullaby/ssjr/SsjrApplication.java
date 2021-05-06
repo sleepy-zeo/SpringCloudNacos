318307f98b529d89a260e7840932530d22f8f00d
wuhongwei <hongwei.wu@grandsun.com>
2021-05-06 15:57:03 +0800

    ssjr

---
+++ sp-shiro-jwt-redis/src/main/java/com/lullaby/ssjr/SsjrApplication.java
@@ -0,0 +1,12 @@
+package com.lullaby.ssjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsjrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsjrApplication.class, args);
+    }
+}
