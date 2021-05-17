34b326faf87f6d09a2949deea7b1105d806f4833
Vaadhoo <steven199409@outlook.com>
2021-05-18 03:15:20 +0800

    support

--- scn-spring-boot-websocket/src/main/java/com/lullaby/ws/WsApplication.java
+++ scn-spring-boot-websocket/src/main/java/com/lullaby/ws/WsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WsApplication {
 
+    // https://ask.csdn.net/questions/687480
+    // https://blog.csdn.net/shadow_zed/article/details/109782040
     public static void main(String[] args) {
         SpringApplication.run(WsApplication.class, args);
     }
