73db905008d6be1a39719ef4f873b2a143f74d15
wuhongwei <hongwei.wu@grandsun.com>
2021-06-30 14:24:18 +0800

    xx

--- scn-cache/src/main/java/com/mogu/cache/domain/User.java
+++ scn-cache/src/main/java/com/mogu/cache/domain/User.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@TableName("user")
+@TableName("tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1090414879337379481L;
