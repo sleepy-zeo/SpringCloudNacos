f5410e58241e2596e13155dbfa6ca41544167ad5
wuhongwei <hongwei.wu@grandsun.com>
2021-07-09 15:22:16 +0800

    generator

---
+++ duck-mybatis-generator/src/main/java/com/duck/mg/MgApplication.java
@@ -0,0 +1,12 @@
+package com.duck.mg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgApplication.class, args);
+    }
+}
---
+++ duck-mybatis-generator/src/main/java/com/duck/mg/config/DruidConfig.java
@@ -0,0 +1,56 @@
+package com.duck.mg.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druidDataSource() {
+        DataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+
+    // 配置Druid Monitor
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> statViewServlet() {
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+
+        // 这些参数可以在com.alibaba.druid.support.http.ResourceServlet中找到
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "1994");
+        initParams.put("allow", "");
+        initParams.put("deny", "192.168.10.132");
+
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+    // 配置Druid Monitor中的URI监控
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> webStatFilter() {
+        FilterRegistrationBean<WebStatFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js, *.css, /druid, /druid/*");
+        bean.setInitParameters(initParams);
+
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
+}
